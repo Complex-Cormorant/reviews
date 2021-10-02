982f6cbad53b325b9b48fcce2c31457694e3d2b5
sparangan <savanna.parangan@gmail.com>
2021-10-02 15:58:53 -0700

    Add getReviewsAndPhotos function

--- database/queries.js
+++ database/queries.js
@@ -11,6 +11,50 @@ module.exports = {
         res.send(results.rows)
       }
     })
+  },
 
+  // want to join
+
+  getReviewsAndPhotos: (req, res) => {
+    const id = (req.query.product_id)
+    pool.query("SELECT reviews.id, product_id, rating, date, summary, body, recommend, reported, reviewer_name, reviewer_email, response, helpfulness, ARRAY_AGG ('id: ' || review_photos.id ||', url: ' || review_photos.url) photos FROM reviews INNER JOIN review_photos ON reviews.id = review_photos.review_id WHERE (product_id = $1) GROUP BY reviews.id;", [id], (error, results) => {
+      if (error) {
+        res.send(error);
+      } else {
+        res.send(results.rows)
+      }
+    })
   }
+
+
+//   addReview: (req, res) => {
+
+//   }
+
+//   markHelpful: (req, res) => {
+
+//   }
+
+//   report: (req, res) => {
+
+//   }
 }
+
+
+
+
+// SELECT
+// reviews.id, product_id, rating, date, summary, body, recommend, reported, reviewer_name, reviewer_email, response, helpfulness, ARRAY_AGG ('id: ' || review_photos.id ||', url: ' || review_photos.url) photos
+// FROM
+// reviews
+// INNER JOIN
+// review_photos
+// ON
+// reviews.id = review_photos.review_id
+// WHERE
+// (product_id = 14)
+// GROUP BY
+// reviews.id
+// ;
+
+// ', review_id: ' || review_photos.review_id ||
--- server/index.js
+++ server/index.js
@@ -4,18 +4,25 @@ var path = require('path');
 const port = 3005
 const db = require('../database/index.js');
 const queries = require('../database/queries.js');
-// TODO:
-// figure out what route in get should be
-// figure out what req.params looks like and what i need to get reviews by id
 // install morgan
+
 app.use(express.static((path.join(__dirname + 'public'))));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('hullo world');
-})
+// app.get('/', (req, res) => {
+//   res.send('hullo world');
+// })
 
-app.get('/api/reviews', queries.getReviews)
+app.get('/api', queries.getReviews)
+
+// TODO:
+app.get('/api/reviews', queries.getReviewsAndPhotos)
+
+// app.post('/api/reviews', queries.addReview)
+
+// app.put('/api/reviews/:review_id/helpful', queries.markHelpful)
+
+// app.put('/api/reviews/:review_id/report', queries.report)
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`)
