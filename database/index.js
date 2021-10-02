bd9bce8561f19f2b7493ee3e54f26caa0c00fd3e
sparangan <savanna.parangan@gmail.com>
2021-10-01 23:23:26 -0700

    Connect db to express server

---
+++ database/index.js
@@ -0,0 +1,32 @@
+var { Pool } = require('pg')
+var db = new Pool({
+  user: 'savanna',
+  password: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  database: 'reviews'
+})
+
+db.connect()
+  .then(() => {
+    console.log('Connected to database');
+  })
+  // .then(() => {
+  //   // use count coming in, if undefined do deafult 5 (req.query)
+  //   db.query('select * from reviews where reviews.product_id = 15 limit 5;', (error, response) => {
+  //     if (error) {
+  //       console.log('Error in query', error);
+  //     } else {
+  //       console.log(response);
+  //       db.end()
+  //     }
+  //   });
+  // })
+  .catch((error) => {
+    console.log('Error connecting to database: ', error);
+  });
+
+  module.exports = {
+    db: db
+  }
+
--- server/index.js
+++ server/index.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express()
-const port = 3000
+var path = require('path');
+const port = 3005
+const db = require('../database/index.js');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// TODO:
+// figure out what route in get should be
+// figure out what req.params looks like and what i need to get reviews by id
+// install morgan
+app.use(express.static((path.join(__dirname + 'public'))));
+app.use(express.json());
+app.use(express.urlencoded());
+// need to figure out how to handle id in the url
+app.get('/api/reviews', (req, res) => {
+  // console.log(req);
+  // db.query()
+  res.send(req.params);
 })
 
 app.listen(port, () => {
